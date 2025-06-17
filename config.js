3d1869c435161a7fe20c7feaf2fd08194baf0d96
Jacopo12344 <jacopulcinopro@gmail.com>
2025-06-17 02:57:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = "" //Inserisci nelle apici il numero del bot (es. +3937620304
 global.confirmCode = ""
 
 global.owner = [
-    ['393509368693', 'Proprietario', true ],
+    ['config.js', 'Proprietario', true ],
 ] //Numero degli owner
 
 //────────────────────────────
